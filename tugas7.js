5093e0cc41907ab1bca32f68a3ca90bc120215cc
Bayu-aditiya-saputra <bayurikasolihat@gmail.com>
2022-01-05 22:05:44 +0700

    tugas 7 js
    tugas untuk niomic

---
+++ tugas7.js
@@ -0,0 +1,10 @@
+var tinggibadan = [45,50,55,60,65,70,75,80,85,90];
+
+for(var i = 0; i < tinggibadan.length; i++){
+    console.log(tinggibadan[i])
+}
+console.log("");
+
+for(i of tinggibadan){
+    console.log(i);
+}
